fe25415f79bef4864bf602e6bba1a922027fc21b
Kai Shao <grepug@icloud.com>
2025-02-02 14:47:37 +0800

    rename package from "ConcurrencyUtils" to "concurrency-utils"

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ConcurrencyUtils",
+    name: "concurrency-utils",
     platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
