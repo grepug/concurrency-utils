e35db04e36d02414c18b48f3af3f49c8269678cc
Kai Shao <grepug@icloud.com>
2025-02-12 15:55:03 +0800

    feat: enhance withCleanup function to support error mapping

--- Sources/ConcurrencyUtils/Utils/withCleanup.swift
+++ Sources/ConcurrencyUtils/Utils/withCleanup.swift
@@ -1,10 +1,15 @@
-public func withCleanup<T>(handler: @escaping () async throws -> T, cleanup: @escaping () async -> Void) async throws -> T {
+public func withCleanup<T>(handler: @escaping () async throws -> T, cleanup: @escaping () async -> Void, mapError: ((Error) -> Error)? = nil) async throws -> T {
     do {
         let result = try await handler()
         await cleanup()
         return result
     } catch {
         await cleanup()
+
+        if let mapError {
+            throw mapError(error)
+        }
+
         throw error
     }
 }
