b9e6580a59916d76fb1552564b6919932f6058bd
Kai Shao <grepug@icloud.com>
2026-01-15 11:54:52 +0800

    fix: update swift-collections dependency version in Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/grepug/event-source.git", branch: "master"),
-        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMinor(from: "1.1.0")),
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
     ],
     targets: [
